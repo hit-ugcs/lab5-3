dceac4083a3300e6ac1a935500aba52dfae23502
Haposola <haposola@gmail.com>
2014-10-25 15:37:51 +0800

    new pro

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ttt::Application.config.secret_token = 'c07e9181abe7c798e4abdd64b3ce0057e6ca6bb4f3ed2fc7cfdb60c0489e921d2fb90d066c9f6f00d80cf0689ce68a3d1685db507782986df9862116eb725c98'
